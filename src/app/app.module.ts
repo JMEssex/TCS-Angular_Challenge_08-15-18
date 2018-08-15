1efea7084b2219794651f94237d286e7945ccad9
James Essex <JMEssex@gmail.com>
2018-08-15 03:54:01 -0500

    generated user-fetch, user-details, and user-details/user-transaction components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserFetchComponent } from './user-fetch/user-fetch.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserTransactionsComponent } from './user-details/user-transactions/user-transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserFetchComponent,
+    UserDetailsComponent,
+    UserTransactionsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-fetch/user-fetch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-fetch',
+  templateUrl: './user-fetch.component.html',
+  styleUrls: ['./user-fetch.component.css']
+})
+export class UserFetchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
